8512a90973ff52737853d1fb4febe2b69dfe1983
Misai Legara <99393784+iammisai@users.noreply.github.com>
2024-04-18 02:29:41 +0800

    Update index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -40,7 +40,7 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, Png, description}: FeatureItem) {
+function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
