142d33d22dbff0e9fa8fa47cc0ec94809e321fd4
Misai Legara <99393784+iammisai@users.noreply.github.com>
2024-04-18 01:56:54 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'Official Documentation for Crashr Platform',
-  tagline: '$CRASHR ISPO IS LIVE',
+  tagline: 'An advanced digital asset trading platform.',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
