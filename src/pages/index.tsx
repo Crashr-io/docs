25fb61be68558acf5d56527c2725cb5d518c9faf
Crashr <146051245+CRASHRPlatform@users.noreply.github.com>
2024-04-23 16:43:18 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,8 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg">
+            className="button button--secondary button--lg"
+            to="docs/Get Started/Readme.md">
             Crashr Overview
           </Link>
         </div>
