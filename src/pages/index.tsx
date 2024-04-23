d2f2b7bc7d3f1e055217844c69e6e83e7380cccc
Crashr <146051245+CRASHRPlatform@users.noreply.github.com>
2024-04-23 16:36:24 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg">
-            Crashr Documentation
+            Crashr Overview
           </Link>
         </div>
       </div>
