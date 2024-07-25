dd519e27de3227e9adc879c04d062a4be50145a4
Crashr <146051245+CRASHRPlatform@users.noreply.github.com>
2024-07-25 07:35:55 -0700

    Update index.tsx

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -21,7 +21,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Collect',
-    Svg: require('@site/static/img/Collect4.svg').default,
+    Svg: require('@site/static/img/Collect.svg').default,
     description: (
       <>
         Bringing digital asset diversification to another level, collect all on-chain assets and showcase them to your community.
@@ -31,7 +31,7 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Connect',
-    Svg: require('@site/static/img/connect3.svg').default,
+    Svg: require('@site/static/img/Connect.svg').default,
     description: (
       <>
         Elevate the peer to peer economy by connecting with others and trade up to your dream digital asset.  
